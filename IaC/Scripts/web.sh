c76f7bf7db51a1fb6474e6f94fac8f2af08a3444
SebastianUrbano <john.urbano@correo.icesi.edu.co>
2020-09-09 21:34:52 -0500

    Initial configuration managment

---
+++ IaC/Scripts/web.sh
@@ -0,0 +1,13 @@
+##!/bin/bash
+sudo yum update -y
+sudo yum install -y git 
+# Clone our repository
+sudo git clone https://github.com/SebastianUrbano/ds-exams.git
+# Install SaltStack
+sudo curl -L https://bootstrap.saltstack.com -o bootstrap_salt.sh
+sudo sh bootstrap_salt.sh
+#Put custom minion config in place (for enabling masterless mode)
+sudo cp -r /srv/ds-exams/ConfigurationManagment/minion.d /etc/salt/
+echo -e 'grains:\n roles:\n  - web' | sudo tee /etc/salt/minion.d/grains.conf
+# Doing provision with saltstack
+sudo salt-call state.apply
