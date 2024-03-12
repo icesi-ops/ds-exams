5b3abdd9ef82ac53b64b6c7e8bc0750cd41d351e
Amilcar-Steban <amilcarshm@gmail.com>
2024-03-12 01:42:02 -0500

    fixes

--- backend/app/src/main/java/com/distribuidos/app/app.java
+++ backend/app/src/main/java/com/distribuidos/app/app.java
@@ -1,26 +1,26 @@
 package com.distribuidos.app;
+
 import jcifs.smb.SmbFile;
 import jcifs.smb.SmbFileOutputStream;
-import spark.Request;
-import spark.Response;
 import spark.Spark;
 import com.google.gson.Gson;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.http.Part;
-import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 public class app {
-    private static final String SAMBA_SERVER = "smb://172.18.0.2/storage_samba";
+    private static final String SAMBA_SERVER = "smb://sambadb/storage_samba";
     private static final String USERNAME = "user_samba";
     private static final String PASSWORD = "pass123.";
-    private static final String DOMAIN = "WORKGROUP";
     private static final Gson gson = new Gson();
+
     public static void main(String[] args) {
         Spark.port(4567);
         Spark.externalStaticFileLocation("/public");
+
+        // Configuración de CORS
         Spark.options("/*", (request, response) -> {
             String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
             if (accessControlRequestHeaders != null) {
@@ -40,22 +40,21 @@ public class app {
             response.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
             response.type("application/json");
         });
+
+        // Endpoint para listar archivos PDF
         Spark.get("/get_file_list", (request, response) -> {
-            try {
-                String folderPath = "/home/steb/distribuidos/data"; // Ruta del directorio
-                File folder = new File(folderPath);
-                String[] fileList = folder.list(); // Lista todos los archivos
-                if (fileList != null) {
-                    return gson.toJson(Arrays.asList(fileList)); // Convierte la lista a JSON
-                } else {
-                    return gson.toJson(new ArrayList<>()); // Devuelve una lista vacía si el directorio no tiene archivos
-                }
-            } catch (Exception e) {
+            List<String> fileList = getFileListFromSamba();
+
+            if (fileList == null) {
                 response.status(500);
-                return "Error al obtener la lista de archivos: " + e.getMessage();
+                return "No se pudo listar los archivos desde Samba.";
+            }
+            else{
+                return gson.toJson(fileList);
             }
         });
 
+        // Endpoint para subir archivos
         Spark.post("/upload", (request, response) -> {
             request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
             try {
@@ -63,14 +62,39 @@ public class app {
                 String fileName = getFileName(filePart);
                 InputStream fileStream = filePart.getInputStream();
                 uploadToSamba(fileStream, fileName);
-                return "Archivo cargado y almacenado en Samba exitosamente";
+                return "Archivo cargado y almacenado en Samba exitosamente.";
             } catch (Exception e) {
                 response.status(500);
                 return "Error al cargar el archivo en Samba: " + e.getMessage();
             }
         });
     }
+
+    private static List<String> getFileListFromSamba() {
+        List<String> fileList = new ArrayList<>();
+
+        try {
+            String authString = USERNAME + ":" + PASSWORD + "@";
+            String sambaUrl = "smb://" + authString + SAMBA_SERVER.substring(6);
+            SmbFile smbFile = new SmbFile(sambaUrl);
+
+            if (smbFile.isDirectory()) {
+                SmbFile[] files = smbFile.listFiles();
+                for (SmbFile file : files) {
+                    fileList.add(file.getName().replace("storage_samba", ""));
+                }
+            } else {
+                System.err.println("La ruta en Samba no es un directorio.");
+            }
+        } catch (Exception e) {
+            System.err.println("Error al obtener la lista de archivos desde Samba: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return fileList;
+    }
+
     private static void uploadToSamba(InputStream fileStream, String fileName) throws Exception {
+        // La URL de Samba debe incluir la información de autenticación antes del servidor
         String authString = USERNAME + ":" + PASSWORD + "@";
         String sambaUrl = "smb://" + authString + SAMBA_SERVER.substring(6) + "/" + fileName;
         System.out.println("Samba URL: " + sambaUrl);
@@ -82,13 +106,16 @@ public class app {
                 smbFileOutputStream.write(buffer, 0, bytesRead);
             }
         } catch (jcifs.smb.SmbException e) {
+            // Maneja específicamente las excepciones de SmbException
             System.err.println("Error al conectar con el servidor Samba: " + e.getMessage());
             e.printStackTrace();
         } catch (Exception e) {
+            // Maneja otras excepciones
             System.err.println("Error al cargar el archivo en Samba: " + e.getMessage());
             e.printStackTrace();
         }
     }
+
     private static String getFileName(Part part) {
         for (String content : part.getHeader("content-disposition").split(";")) {
             if (content.trim().startsWith("filename")) {
